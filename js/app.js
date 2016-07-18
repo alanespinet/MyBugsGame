cd67499261faf5e5f607cfdc126242b75d8a7c2f
alanespinet <aelluvetaa@gmail.com>
2016-07-18 16:02:21 -0700

    newUpdate

--- js/app.js
+++ js/app.js
@@ -17,79 +17,86 @@ var sprites = [
 
 var newSprite = -1;
 
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+class Enemy{
+  constructor(x, y, speed){
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
     this.speed = speed;
-};
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  update(dt){
     this.x += this.speed * dt;
     if(this.x >= 520)
     {
       this.x = -100;
       this.speed = (Math.random() * 1000) + 50;
     }
-};
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  render(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
+
+class Gem {
+  constructor(x, y){
+    this.sprite = 'images/Gem Blue.png';
+    this.x = x;
+    this.y = y;
+  }
+
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
+
+var gem = new Gem(0, 0);
+
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y){
-  this.sprite = 'images/char-boy.png';
-  this.x = x;
-  this.y = y;
+class Player {
+  constructor(x, y){
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+  }
+
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  handleInput(key){
+    if(key == 'left') this.x -= 101;
+    if(key == 'right') this.x += 101;
+    if(key == 'up') this.y -= 83;
+    if(key == 'down') this.y += 83;
+  }
+
+  update(){
+    if(player.x > 407) player.x = 407;
+    if(player.x < 3) player.x = 3;
+    if(player.y > 380) player.y = 380;
+
+    if(player.y < 48) {
+      player.y = 380;
+      wins++;
+      document.getElementById("wins").innerHTML = "Wins: " + wins;
+    }
+
+    if(checkForCollisions())
+    {
+      player.x = 205;
+      player.y = 380;
+
+      loses++;
+      document.getElementById("loses").innerHTML = "Loses: " + loses;
+    }
+  }
 }
-
-
-Player.prototype.render = function(){
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-
-Player.prototype.handleInput = function(key){
-  if(key == 'left') this.x -= 101;
-  if(key == 'right') this.x += 101;
-  if(key == 'up') this.y -= 83;
-  if(key == 'down') this.y += 83;
-};
-
-Player.prototype.update = function(){
-  if(player.x > 407) player.x = 407;
-  if(player.x < 3) player.x = 3;
-  if(player.y > 380) player.y = 380;
-
-  if(player.y < 48) {
-    player.y = 380;
-    wins++;
-    document.getElementById("wins").innerHTML = "Wins: " + wins;
-  }
-
-  if(checkForCollisions())
-  {
-    player.x = 205;
-    player.y = 380;
-
-    loses++;
-    document.getElementById("loses").innerHTML = "Loses: " + loses;
-  }
-};
 
 function checkForCollisions(){
   for(var i = 0; i < allEnemies.length; i++){
@@ -120,9 +127,5 @@ document.addEventListener('keyup', function(e) {
 
     moves++;
     document.getElementById("moves").innerHTML = "Moves: " + moves;
-
-    newSprite = Math.floor(Math.random() * 10);
-    player.sprite = sprites[newSprite];
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
